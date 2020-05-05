7b85e7371f6eaaf702c7d7eac7c4420da8558695
yulean <yuleanrios@outlook.com>
2020-05-05 10:05:43 -0500

    ososososososscambioososos

--- ArbolZipa.py
+++ ArbolZipa.py
@@ -1,6 +1,7 @@
+import pandas as pd
 import h2o
 import os
-import pandas as pd
+
 import numpy as np
 import discretizacion as dz
 import subprocess
@@ -29,7 +30,7 @@ class ArbolZipa():
         
         h2o.init(max_mem_size = "2G")
         h2o.remove_all() 
-        df = pd.read_excel('C:/Users/yulea/Documents/pruebasPY/p18.xlsx',encoding="ISO-8859-1")
+        df = pd.read_excel('p18.xlsx',encoding="ISO-8859-1")
         umbrales =np.linspace(0, 1, 7)
         df = df.replace(np.nan, None)
         covtype_df=h2o.H2OFrame(self.discretizador.discretizar(self.listaVarDis,umbrales,df))
@@ -57,10 +58,27 @@ class ArbolZipa():
         img.generateTree(h2o_jar_path, mojo_full_path, gv_file_path, image_file_name, 3)
         img.generateTreeImage(gv_file_path, image_file_name, 3)
 
-    def predecir(self):
-        print(self.test)
+    def entrenamiento(self,opcion,archivo):
+        if opcion=="archivoLocal":
+            self.crear_arbol()
+        else:
+            df = pd.read_excel(archivo,encoding="ISO-8859-1")
+            umbrales =np.linspace(0, 1, 7)
+            df = df.replace(np.nan, None)
+            covtype_df=h2o.H2OFrame(self.discretizador.discretizar(self.listaVarDis,umbrales,df))
+            covtype_df=covtype_df.drop([0], axis=0)
+            covtype_df["T"]=covtype_df["T"].asfactor()
+            train,x = covtype_df.split_frame([0.8], seed=56478)
+            valid,x=covtype_df.split_frame([0.5],seed=56478)
+            covtype_X = covtype_df.col_names[:-1]     
+            covtype_y = covtype_df.col_names[-1] 
+            self.rf_v2.train(x=covtype_X, y=covtype_y, training_frame=train,validation_frame=valid)
+
+
+    def predecir(self,test):
+        print(test)
         print("________________________________________________________________________________________________________________")
-        final_rf_predictions = self.rf_v2.predict(self.test[:-1])
+        final_rf_predictions = self.rf_v2.predict(test[:-1])
         df_predict=final_rf_predictions.as_data_frame(use_pandas=True)
         print(self.discretizador.get_valT())
         nuevaColumna=self.discretizador.getIntervalo(self.discretizador.get_valT(),df_predict["predict"])
@@ -70,10 +88,21 @@ class ArbolZipa():
         nuevaColumna=h2o.H2OFrame(nuevaColumna)
         #self.test=self.test.as.data_frame(use_pandas=True)
 
-        self.test["t"] = nuevaColumna["T"]
-        print(self.test)
+        test["t"] = nuevaColumna["T"]
+        print(test)
+
 if __name__ =="__main__":
     arbol=ArbolZipa()
     arbol.crear_arbol()
     arbol.dibujarArbol()
-    arbol.predecir()
+    test=pd.read_excel('p18.xlsx',encoding="ISO-8859-1")
+    test = test.replace(np.nan, None)
+    dftest=h2o.H2OFrame(test)
+    dftest=dftest.drop([0],axis=1)
+    dftest=dftest.drop([0],axis=0)
+    print(dftest)
+    dftest=dftest.drop(["T"],axis=1)
+    print(dftest)
+    arbol.predecir(dftest)
+    arbol.entrenamiento("","p18.xlsx")
+    
--- ImageDef.py
+++ ImageDef.py
@@ -2,21 +2,27 @@ import subprocess
 from IPython.display import Image
 
 def generateTreeImage(gv_file_path, image_file_path, tree_id):
-    image_file_path = image_file_path + "_" + str(tree_id) + ".png"
-    result = subprocess.call(["dot", "-Tpng", gv_file_path, "-o", image_file_path], shell=False)
-    print(result)
-    if result is 0:
-        print("Success: Image File " + image_file_path + " is generated.")
-        print("Now you can execute the follow line as-it-is to see the tree graph:") 
-        print("Image(filename='" + image_file_path + "\')")
-    else:
-        print("Error: Image file " + image_file_path + " could not be generated.")
+    try:
+        image_file_path = image_file_path + "_" + str(tree_id) + ".png"
+        result = subprocess.call(["dot", "-Tpng", gv_file_path, "-o", image_file_path], shell=False)
+        print(result)
+        if result is 0:
+            print("Success: Image File " + image_file_path + " is generated.")
+            print("Now you can execute the follow line as-it-is to see the tree graph:") 
+            print("Image(filename='" + image_file_path + "\')")
+        else:
+            print("Error: Image file " + image_file_path + " could not be generated.")
+    except:
+        pass
 
 def generateTree(h2o_jar_path, mojo_full_path, gv_file_path, image_file_path, tree_id = 0):
-    image_file_path = image_file_path + "_" + str(tree_id) + ".png"
-    result = subprocess.call(["java", "-cp", h2o_jar_path, "hex.genmodel.tools.PrintMojo", "--tree", str(tree_id), "-i", mojo_full_path ,"-o", gv_file_path ], shell=False)
-    print(result)
-    if result is 0:
-        print("Success: Graphviz file " + gv_file_path + " is generated.")
-    else: 
-        print("Error: Graphviz file " + gv_file_path + " could not be generated.")
+    try:
+        image_file_path = image_file_path + "_" + str(tree_id) + ".png"
+        result = subprocess.call(["java", "-cp", h2o_jar_path, "hex.genmodel.tools.PrintMojo", "--tree", str(tree_id), "-i", mojo_full_path ,"-o", gv_file_path ], shell=False)
+        print(result)
+        if result is 0:
+            print("Success: Graphviz file " + gv_file_path + " is generated.")
+        else: 
+            print("Error: Graphviz file " + gv_file_path + " could not be generated.")
+    except:
+        pass
